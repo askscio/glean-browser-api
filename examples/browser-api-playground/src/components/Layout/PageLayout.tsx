37c9ad1eed2f9128c0a14264a4d8ef6400793077
naveen-ls-glean <naveen.ls@glean.com>
2023-08-10 14:16:52 +0530

    create a new example

---
+++ examples/browser-api-playground/src/components/Layout/PageLayout.tsx
@@ -0,0 +1,23 @@
+import { PropsWithChildren, useMemo, useState } from "react";
+import Header from "./Header";
+import { EmbedConfigContext } from "../../EmbedConfigContext";
+import useConfigStore from "../../useConfigStore";
+
+const PageLayout = ({ children }: PropsWithChildren) => {
+  const contextValue = useConfigStore()
+
+  return <EmbedConfigContext.Provider value={contextValue}>
+    <div className="w-full h-full flex flex-col items-center">
+      <div className="w-fixed w-full flex-shrink flex-grow-0 px-4 w-fixed w-full flex-shrink flex-grow-0 px-4 border-b border-gray-20 shadow-md">
+        <div className="sticky top-0 p-4 w-full h-full z-50">
+          <Header />
+        </div>
+      </div>
+      <main role="main" className="w-full flex-auto">
+        {children}
+      </main>
+    </div>
+  </EmbedConfigContext.Provider>
+};
+
+export default PageLayout;
--- examples/search-chat-separate-routes/src/components/Layout/PageLayout.tsx
+++ examples/search-chat-separate-routes/src/components/Layout/PageLayout.tsx
@@ -1,23 +1,17 @@
-import { PropsWithChildren, useMemo, useState } from "react";
+import { PropsWithChildren } from "react";
 import Header from "./Header";
-import { EmbedConfigContext } from "../../EmbedConfigContext";
-import useConfigStore from "../../useConfigStore";
 
-const PageLayout = ({ children }: PropsWithChildren) => {
-  const contextValue = useConfigStore()
-
-  return <EmbedConfigContext.Provider value={contextValue}>
-    <div className="w-full h-full flex flex-col items-center">
-      <div className="w-fixed w-full flex-shrink flex-grow-0 px-4 w-fixed w-full flex-shrink flex-grow-0 px-4 border-b border-gray-20 shadow-md">
-        <div className="sticky top-0 p-4 w-full h-full z-50">
-          <Header />
-        </div>
+const PageLayout = ({ children }: PropsWithChildren) => (
+  <div className="w-full h-full flex flex-col items-center">
+    <div className="w-fixed w-full flex-shrink flex-grow-0 px-4 w-fixed w-full flex-shrink flex-grow-0 px-4 border-b border-gray-20 shadow-md">
+      <div className="sticky top-0 p-4 w-full h-full z-50">
+        <Header />
       </div>
-      <main role="main" className="w-full flex-auto">
-        {children}
-      </main>
     </div>
-  </EmbedConfigContext.Provider>
-};
+    <main role="main" className="w-full flex-auto">
+      {children}
+    </main>
+  </div>
+);
 
 export default PageLayout;
