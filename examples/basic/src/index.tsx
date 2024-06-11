d47c0704ee6290f57d27a43151d05589e66d242d
Tao Zhou <tao.zhou@glean.com>
2024-06-11 23:35:00 +0000

    add a basic example that can set up glean integration within a single file

---
+++ examples/basic/src/index.tsx
@@ -0,0 +1,64 @@
+import React, { useRef } from "react";
+import { createRoot } from "react-dom/client";
+import { Divider, Input, Select, Typography } from "antd";
+import { useEffect, useState } from "react";
+import type { EmbeddedSearch } from "@gleanwork/web-sdk";
+
+import "./styles.css";
+
+declare global {
+  interface Window {
+    EmbeddedSearch?: EmbeddedSearch;
+  }
+}
+
+function App() {
+  const containerRef = useRef(null);
+  const [isGleanReady, setIsGleanReady] = useState(false);
+
+  useEffect(() => {
+    if (!isGleanReady || !window.EmbeddedSearch || !containerRef.current)
+      return;
+
+    window.EmbeddedSearch.attach(containerRef.current, {
+      onSearch(query) {
+        console.log(query);
+      },
+    });
+  }, [isGleanReady]);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://app.glean.com/embedded-search-latest.min.js";
+    script.onload = () => {
+      setIsGleanReady(true);
+    };
+    document.head.appendChild(script);
+  }, []);
+
+  return (
+    <div>
+      <Typography.Paragraph>
+        Basic Glean Search Widget Setup
+      </Typography.Paragraph>
+      <input
+        ref={containerRef}
+        className="border-solid border-4 rounded-2xl"
+        placeholder="Search"
+        style={{
+          padding: 15,
+          height: 40,
+          width: 220,
+        }}
+      />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root") as HTMLElement;
+
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
